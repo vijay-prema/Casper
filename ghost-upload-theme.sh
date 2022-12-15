43c10d2a961540a7e072ebb1573d076ac61110ca
vijay-prema <vjprema@gmail.com>
2022-12-15 22:16:42 +1300

    Add badges and auto upload script

---
+++ ghost-upload-theme.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Please define the following env variables:
+
+# GHOST_ADMIN_API_URL=https://yourblog.com
+# GHOST_LOGIN_EMAIL=your@email.com
+# GHOST_LOGIN_PASS=your-secure-login-password
+
+yarn zip
+
+curl -c ghost-cookie.txt -d username=$GHOST_LOGIN_EMAIL -d password=$GHOST_LOGIN_PASS \
+   -H "Accept-Version: v5.0" \
+   -H "Origin: $GHOST_ADMIN_API_URL" \
+   $GHOST_ADMIN_API_URL/ghost/api/admin/session/
+
+curl -X POST -b ghost-cookie.txt \
+   -F 'file=@dist/casper-vijay.zip' \
+   -H "Accept-Version: v5.0" \
+   -H "Origin: $GHOST_ADMIN_API_URL" \
+   $GHOST_ADMIN_API_URL/ghost/api/admin/themes/upload/
